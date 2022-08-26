23b3c62c84fecd855b03354f0eebff092143c718
Akhil Shaji <98893138+thedevakhil@users.noreply.github.com>
2022-08-26 11:48:26 +0530

    initializing values to array using constructor

---
+++ Array_using_Constructor.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Student {
+  String name;
+  int mark;
+  Student(String n, int m) {
+    name = n;
+    mark = m;
+  }
+
+  public void printdetails() {
+    System.out.println("Name :" + name);
+    System.out.println("Marks :" + mark);
+  }
+}
+class Test2 {
+  public static void main(String args[]) {
+    Student[] st = new Student[5];
+    Scanner s = new Scanner(System.in);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Student No: " + (i + 1));
+      System.out.println("Enter Name :");
+      String name = s.next();
+      System.out.println("Enter Mark : ");
+      int mark = s.nextInt();
+      st[i] = new Student(name, mark);
+
+    }
+    for (int i = 0; i < 5; i++) {
+      st[i].printdetails();
+    }
+  }
+}
